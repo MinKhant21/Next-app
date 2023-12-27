79be0f3e5da82d30c802bf3b3035c9eeea7e9711
KaungMinKhant <kaungminkhant.edka@gmail.com>
2023-12-27 14:42:13 +0630

    prisma client get user data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,13 +1,10 @@
 import { NextRequest, NextResponse } from "next/server"
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export const GET = () => {
-     return NextResponse.json([
-          {
-               id:1,
-               name : 'kmk',
-          }
-     ])
+export const GET =  async() => {
+     let data = await prisma.user.findMany()
+     return NextResponse.json(data)
 }
 
 export const POST = async (request:NextRequest) => {
