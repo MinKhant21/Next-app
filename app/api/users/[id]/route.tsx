95c27c743bbc2c1d0bab94d2d89a5207e52e7497
KaungMinKhant <kaungminkhant.edka@gmail.com>
2023-12-27 13:30:57 +0630

    user detail api

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET (request:NextRequest,{params}:{params:{id:number}}) {
+     console.log(params.id)
+     if(params.id > 10){
+          return NextResponse.json({error:"User not Found"},{status:404})
+       }
+     return NextResponse.json({id:1,name:'kmkget'})
+}
