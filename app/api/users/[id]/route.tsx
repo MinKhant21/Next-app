bc3966a10defee35f9ad80ff4b547d3974ddc8c7
KaungMinKhant <kaungminkhant.edka@gmail.com>
2023-12-27 13:41:15 +0630

    user api crud

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -7,3 +7,24 @@ export function GET (request:NextRequest,{params}:{params:{id:number}}) {
        }
      return NextResponse.json({id:1,name:'kmkget'})
 }
+
+export async function PUT (request:NextRequest,{params}:{params:{id:number}}) {
+     const body = await request.json();
+     if(!body.name)
+          return NextResponse.json({error:"Empty not Found name"},{status:404})
+     if(params.id > 10){
+          return NextResponse.json({error:"User not Found"},{status:404})
+       }
+     return NextResponse.json({id:1,name:body.name})
+}
+
+
+export async function DELETE (request:NextRequest,{params}:{params:{id:number}}) {
+     const body = await request.json();
+     if(!body.name)
+          return NextResponse.json({error:"Empty not Found name"},{status:404})
+     if(params.id > 10){
+          return NextResponse.json({error:"User not Found"},{status:404})
+       }
+     return NextResponse.json({id:1,name:body.name} )
+}
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 export const GET = () => {
      return NextResponse.json([
@@ -7,4 +7,11 @@ export const GET = () => {
                name : 'kmk',
           }
      ])
+}
+
+export const POST = async (request:NextRequest) => {
+     const body = await request.json(); 
+     if(!body.name)
+          return NextResponse.json({error:"Cannt find Name"})
+     return NextResponse.json({id:1,name:body.name})
 }
