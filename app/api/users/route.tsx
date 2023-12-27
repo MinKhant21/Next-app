9962bfac2e33ef09f665586cbb39c7c218091fa6
KaungMinKhant <kaungminkhant.edka@gmail.com>
2023-12-27 13:18:56 +0630

    api user get

---
+++ app/api/users/route.tsx
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server"
+
+export const GET = () => {
+     return NextResponse.json([
+          {
+               id:1,
+               name : 'kmk',
+          }
+     ])
+}
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client '
 import React, { Suspense } from 'react'
 import UserTable from '../components/UserTable';
 
