1a9f681df9837ac59e73a1bab562ae9038b65c00
KaungMinKhant <kaungminkhant.edka@gmail.com>
2024-03-28 15:51:04 +0630

    sign in using next auth

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div className='flex p-5 bg-slate-400'>
+    <div className='flex p-5 bg-slate-400 space-x-5'>
      <Link href="/" className=' text-md mr-3'>Next Js</Link>
      <Link href="/users" className=' text-md'>Users</Link>
+     <Link href="/api/auth/signin" className=' text-md'>Sign in</Link>
     </div>
   )
 }
